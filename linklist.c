667a8b8f90124ecef15a9d649426e2c58b8edbd6
Abhirai1 <101519306+Abhirai1@users.noreply.github.com>
2022-10-17 23:23:34 +0530

    Add files via upload

--- linklist.c
+++ linklist.c
@@ -1,62 +1,179 @@
-#include<stdio.h>
+#include<stdio.h> 
 #include<stdlib.h>
-struct Node
-{
+struct node{
+    int info; 
+    struct node *next;
+}; 
+
+struct node *head = NULL; 
+
+void create(){
+    struct node *ptr,*temp; 
+    int data; 
+    temp =(struct node *)malloc(sizeof(struct node) ); 
+    printf("\nEnter the value of information part : ");
+    scanf("%d",&data); 
+    temp->info=data; 
+    temp->next=NULL;
+    if(head==NULL){
+        head=temp;
+    } 
+    else{
+        ptr=head; 
+        while(ptr->next!=NULL){
+            ptr=ptr->next;
+        } 
+        ptr->next=temp;
+    }
+} 
+
+void display(){ 
+    struct node  *ptr; 
+    ptr = head;
+    while(ptr!=NULL){
+        printf("%d \n",ptr->info); 
+        ptr=ptr->next;
+    }
+} 
+
+void insertAtBeg(){
+    struct node *ptr,*temp; 
     int data;
-    struct Node * next
-};
-void traversal(struct Node *ptr)
-{
-    while(ptr!=NULL)
-    {
-    printf("%d ",ptr->data);
-    ptr=ptr->next;
+    temp =(struct node *)malloc(sizeof(struct node) ); 
+    printf("\nEnter the value to be inserted at begening : ");
+    scanf("%d",&data); 
+    temp->info=data; 
+    temp->next=NULL; 
+    // ynha pe add kar rhe hai 
+    ptr=head; 
+    temp->next=ptr;
+    head=temp; 
+
+} 
+
+void insertAtEnd(){
+    struct node *ptr,*temp; 
+    int data;
+    temp =(struct node *)malloc(sizeof(struct node) ); 
+    printf("\nEnter the data to insert at the end position :  "); 
+    scanf("%d",&data); 
+    temp->info=data; 
+    temp->next=NULL; 
+    if(head==NULL){
+        head=temp;
+    } 
+    else{
+        ptr=head; 
+        while(ptr->next!=NULL){
+            ptr=ptr->next;
+        } 
+        ptr->next=temp;
+    }   
+} 
+
+void insertAtKthPosition(){
+    struct node *ptr,*temp; 
+    int data,k,i;
+    temp =(struct node *)malloc(sizeof(struct node) ); 
+    printf("\nEnter the information part : ");
+    scanf("%d",&data); 
+    temp->info=data; 
+    temp->next=NULL; 
+
+    printf("\nEnter the position to insert a node : ");
+    scanf("%d",&k);
+    i=1; 
+    while(i<k-1){
+        ptr=ptr->next;
+        ++i;
+    } 
+    temp->next=ptr->next; 
+    ptr->next=temp;
+} 
+
+void deleteAtBeg(){
+    struct node *ptr; 
+    ptr=head; 
+    if(ptr==NULL){
+        printf("\nUnderflow condition .... "); 
+    } 
+    else{
+        head=ptr->next; 
+        free(ptr); 
+        printf("\nNode deleted at beginning successfully....\n");
+    }
+} 
+
+void deletenodeAtEnd(){
+    struct node *ptr,*preptr; 
+    ptr=head; 
+    if(ptr==NULL){
+        printf("\nUnder flow problem.......\n");
+    } 
+    else if(ptr->next=NULL){
+        head=NULL; 
+        printf("\ndeleted successfully..\n"); 
+        free(ptr);
+    } 
+    else{
+        while(ptr->next!=NULL){
+            preptr=ptr; 
+            ptr=ptr->next;
+        } 
+        preptr->next=NULL; 
+        printf("Deleted successfully...\n");
+        free(ptr);
+    }
+} 
+
+void deletenodeAtkthPos(){
+    struct node *preptr , *ptr; 
+    int i,x,pos;
+    ptr=head; 
+    if(ptr==head){
+        printf("Underflow condition...\n");
+    } 
+    else{ 
+        printf("\nEnter the pos to delete :  "); 
+        scanf("%d",&pos);
+        if(pos==1){
+            head=ptr->next;
+            free(ptr);
+        } 
+        else{
+            i=1; 
+            while(i<pos){
+                preptr=ptr; 
+                ptr=ptr->next;
+                ++i;
+            } 
+            preptr->next=ptr->next; 
+            printf("Deleted successfully...\n"); 
+            free(ptr);
+        }
+
     }
 }
-struct Node * insert(struct Node *head ,int data )
-{
-    struct Node *ptr=(struct Node *)malloc(sizeof(struct Node));
-    ptr->next=head;
-    ptr->data=data;
-    return ptr;
+
+int main(){ 
+    int ch; 
+    while(1){
+        printf("(1:create)  (2:display ) (0:exit)  (3:insertAtBeg)  (4:insertAtEnd)  (5:insertAtKthPosition) (6:deleteAtBeg) (7:deletenodeAtEnd)  (8:deletenodeAtkthPos) \n"); 
+        printf("Enter your choice "); 
+        scanf("%d",&ch); 
+        switch(ch){
+            case 1: create();break;
+            case 2: display();break;
+            case 3: insertAtBeg();break;
+            case 4:  insertAtEnd();break;
+            case 5: insertAtKthPosition();break; 
+            case 6: deleteAtBeg();break;
+            case 7: deletenodeAtEnd();break;
+            case 8: deletenodeAtkthPos();break;
+            case 0: exit(1);break; 
+            default : printf("WA..");
+        }
+    } 
+    return 0;
+
 }
-//  to delete a node at the begging.
-void delete_start(){
-    NODE *temp = head;
-    head = head->next;
-    free(temp);
-}
-
-
-int main()
-{
-    struct Node * head;
-    struct Node * second;
-    struct Node * third;
-    head=(struct Node *)malloc(sizeof(struct Node));
-    second=(struct Node *)malloc(sizeof(struct Node));
-    third=(struct Node *)malloc(sizeof(struct Node));
-    head->data=6;
-    head->next=second;
-    second->data=7;
-    second->next=third;
-    third->data=12;
-    third->next=NULL;
-    traversal(head);
-   head= insert(head,45);
-    printf("\n");
-    traversal(head);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
